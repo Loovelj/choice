fc9aa7bac822ef70bc5356bd0db10f0fe3997bf4
Kang Jing <l>
2020-01-08 18:26:16 +0800

    ADD fetch stock

--- choice.py
+++ choice.py
@@ -1,11 +1,19 @@
 # python 3.7.4
 # coding = utf-8
-# filename utils.py
+# filename choice.py
 # author 463714869@qq.com/www.cdzcit.com,
 #        create by VIM at 2019/12/30
 from datasets import EMQuantErrDict
+from EMQuantAPI import EmQuantAPI
 
 
 def showEMQuantErrMessage(errcode):
     errMessage = EMQuantErrDict.get(errcode, "Unkown error code-未知的错误代码")
     print('%s - %s' % (errcode, errMessage))
+
+
+def choiceLogin():
+    loginResult = EmQuantAPI.c.start()
+    if loginResult.ErrorCode != 0:
+        showEMQuantErrMessage(loginResult.ErrorCode)
+        exit()
--- get_stock_codes_and_attrs.py
+++ get_stock_codes_and_attrs.py
@@ -1,11 +1,11 @@
 # python 3.7.4
 # coding = utf-8
-# filename get_stock_codes.py
+# filename get_stock_codes_and_attrs.py
 # author 463714869@qq.com/www.cdzcit.com,
 #        create by VIM at 2019/12/30
 
 from EMQuantAPI import EmQuantAPI
-from utils import showEMQuantErrMessage
+from choice import showEMQuantErrMessage, choiceLogin
 from datasets import EMQuantCodesList_Concept, EMQuantCodesList_Market, EMQuantCodesList_DongCai
 import time
 import pymysql
@@ -19,10 +19,7 @@ db_name = 'zccm_lhua'
 if __name__ == '__main__':
 
     # 登录Choice
-    loginResult = EmQuantAPI.c.start()
-    if loginResult.ErrorCode != 0:
-        showEMQuantErrMessage(loginResult.ErrorCode)
-        exit()
+    choiceLogin()
 
     # 登录DB
     conn = pymysql.connect(host=db_host,
@@ -32,9 +29,13 @@ if __name__ == '__main__':
                            db=db_name,
                            charset='utf8')
     cursor = conn.cursor()
+    d = cursor.execute('select * from t_stock_market_classes')
+    print(cursor.fetchall())
+    exit()
 
     # 拉取深沪股票信息 - 市场类
     today = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+    """
     for item in EMQuantCodesList_Market:
         for key_1 in item:
             for key in item[key_1]:
@@ -68,7 +69,7 @@ if __name__ == '__main__':
             # 二级分类
             for key_2 in item[key_1]:
                 # 三级分类
-                for key in item[key_2]:
+                for key in item[key_1][key_2]:
                     result = EmQuantAPI.c.sector(key, today)
                     if result.ErrorCode != 0:
                         showEMQuantErrMessage(result.ErrorCode)
@@ -88,21 +89,20 @@ if __name__ == '__main__':
                                    key_1,
                                    key_2,
                                    str(key)[:-3],
-                                   item[key],
+                                   item[key_1][key_2][key],
                                    key)
                         else:
-                            midstr = cursor.fetchone()[0] + ('%s,' % cid)
                             sql = 'UPDATE t_stock SET ' \
                                   'dc_category = "%s",' \
                                   'dc_subcategory_1 = "%s",' \
                                   'dc_subcategory_1_code = "%s",' \
                                   'dc_subcategory_2 = "%s",' \
-                                  'dc_subcategory_2_code = "%s",' \
+                                  'dc_subcategory_2_code = "%s" ' \
                                   'WHERE code = "%s"' % \
                                   (key_1,
                                    key_2,
                                    str(key)[:-3],
-                                   item[key],
+                                   item[key_1][key_2][key],
                                    key,
                                    result.Data[i * 2])
                         cursor.execute(sql)
@@ -117,7 +117,7 @@ if __name__ == '__main__':
                     showEMQuantErrMessage(result.ErrorCode)
                     continue
                 for i in range(len(result.Data) // 2):
-                    # 获取市场分类ID
+                    # 获取概念分类ID
                     res = cursor.execute('SELECT * FROM t_stock_concept_classes where stock_concept_code="%s"' % key)
                     if res != 1:
                         print('Unkown concept code: %s' % key)
@@ -129,11 +129,16 @@ if __name__ == '__main__':
                         sql = 'INSERT INTO t_stock (code, name, concept) VALUES ("%s", "%s", "%s,")' % \
                               (result.Data[i * 2], result.Data[i * 2 + 1], cid)
                     else:
-                        midstr = cursor.fetchone()[0] + ('%s,' % cid)
+                        old_cid = cursor.fetchone()[0]
+                        if old_cid is None:
+                            new_cid = '%s,' % cid
+                        else:
+                            new_cid = "%s%s," % (old_cid, cid)
                         sql = 'UPDATE t_stock SET concept = "%s" WHERE code = "%s"' % \
-                              (midstr, result.Data[i * 2])
+                              (new_cid, result.Data[i * 2])
                     cursor.execute(sql)
                     conn.commit()
+    """
 
     # 拉取股票地域等信息
     res = cursor.execute('SELECT code FROM t_stock')
@@ -141,10 +146,15 @@ if __name__ == '__main__':
         print('Not find any data')
     else:
         code_data = cursor.fetchall()
-        for code in code_data:
+        for i, code in enumerate(code_data):
+            print("%d -> %s" % (i, code[0]))
             # 东财代码,所属地区板块,省份,城市,区县信息,所属东财行业指数,所属东财行业指数代码
-            data = EmQuantAPI.c.css(code, "EMCODE,AREA,PROVINCE,CITY,COUNTYINFORMATION,BLEMIND,BLEMINDCODE",
+            data = EmQuantAPI.c.css(code[0], "EMCODE,AREA,PROVINCE,CITY,COUNTYINFORMATION,BLEMIND,BLEMINDCODE",
                                     "ClassiFication=1")
+            if data.ErrorCode != 0:
+                showEMQuantErrMessage(data.ErrorCode)
+                continue
+
             sql = 'UPDATE t_stock SET ' \
                   'dc_code = "%s",' \
                   'area = "%s",' \
@@ -152,8 +162,11 @@ if __name__ == '__main__':
                   'city = "%s",' \
                   'subcity = "%s",' \
                   'dc_industry_index = "%s",' \
-                  'dc_industry_index_code = "%s"' % (
-                      data[0], data[1], data[2], data[3], data[4], data[5], data[6]
+                  'dc_industry_index_code = "%s" WHERE code = "%s"' % (
+                      data.Data[code[0]][0], data.Data[code[0]][1],
+                      data.Data[code[0]][2], data.Data[code[0]][3],
+                      data.Data[code[0]][4], data.Data[code[0]][5],
+                      data.Data[code[0]][6], code[0]
                   )
             cursor.execute(sql)
             conn.commit()
